c8df75013a63138f0f336be97667b2ff995bd2a4
Dag Odenhall <dag.odenhall@gmail.com>
2011-06-04 03:31:43 +0200

    reorder some methods for consistency

--- tests/app/__init__.py
+++ tests/app/__init__.py
@@ -12,12 +12,12 @@ from tests.app           import objects, endpoints
 
 class TestApp(TransactionMixin, ZODBMixin, GenshiMixin, Application):
 
-    def setup(self):
-        self.storage = DemoStorage()
-        self.route('/', endpoints.index, methods=('GET', 'POST'))
-
     @request_property
     def db(self):
         if 'testapp' not in self.root_object:
             self.root_object['testapp'] = objects.Root()
         return self.root_object['testapp']
+
+    def setup(self):
+        self.storage = DemoStorage()
+        self.route('/', endpoints.index, methods=('GET', 'POST'))
