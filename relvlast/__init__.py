970bbb8a96ef538d837f148bbe1a37b0757eac04
Dag Odenhall <dag.odenhall@gmail.com>
2011-06-05 02:25:22 +0200

    add basic listing and saving of definitions

--- relvlast/__init__.py
+++ relvlast/__init__.py
@@ -1,4 +1,6 @@
 from persistent        import Persistent
+from BTrees.OOBTree    import OOBTree
+from werkzeug.utils    import redirect
 from werkzeug.testapp  import test_app
 
 from ramverk.fullstack import Application
@@ -6,11 +8,29 @@ from ramverk.utils     import request_property
 
 
 class Root(Persistent):
-    pass
+
+    def __init__(self):
+        self.definitions = OOBTree()
+
+
+class Definition(Persistent):
+
+    def __init__(self, word, definition):
+        self.word = word
+        self.definition = definition
 
 
 def index(render):
     return render('index.html')
+
+
+def definitions(request, render, db, path):
+    if request.method == 'GET':
+        return render('definitions.html', definitions=db.definitions)
+    elif request.method == 'POST':
+        definition = Definition(**request.form)
+        db.definitions[definition.word] = definition
+        return redirect(path('definitions'))
 
 
 class Relvlast(Application):
@@ -24,3 +44,4 @@ class Relvlast(Application):
     def setup(self):
         self.route('/__info__', lambda x: test_app, endpoint='__info__')
         self.route('/', index)
+        self.route('/definitions/', definitions, methods=('GET', 'POST'))
