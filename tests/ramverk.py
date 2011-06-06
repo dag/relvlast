b12c1852c647047220674778248af63c718fef2f
Dag Odenhall <dag.odenhall@gmail.com>
2011-06-06 22:45:48 +0200

    test genshi as unit

--- tests/ramverk.py
+++ tests/ramverk.py
@@ -2,7 +2,7 @@ from __future__        import absolute_import
 from textwrap          import dedent
 
 from attest            import Tests, assert_hook
-from werkzeug.test     import Client
+from werkzeug.test     import Client, create_environ
 from werkzeug.wrappers import BaseResponse
 from ZODB.DemoStorage  import DemoStorage
 
@@ -51,6 +51,28 @@ def post_to_and_get_index(client):
         <html>
           <body>
             <h1>Hello</h1>
+            <form method="POST" action="/">
+              <input type="text" name="greeting" placeholder="Enter a greeting">
+            </form>
+          </body>
+        </html>""")
+
+
+genshi = Tests()
+
+@genshi.context
+def fake_wsgi_environ():
+    app.setup_environ(create_environ())
+    yield
+
+@genshi.test
+def render_genshi_template():
+    response = app.render('index.html', greeting='Hi')
+    assert response.data == dedent("""\
+        <!DOCTYPE html>
+        <html>
+          <body>
+            <h1>Hi</h1>
             <form method="POST" action="/">
               <input type="text" name="greeting" placeholder="Enter a greeting">
             </form>
