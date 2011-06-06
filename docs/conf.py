caf46f4fc09c961891db457b4b4d3e2b953b5671
Dag Odenhall <dag.odenhall@gmail.com>
2011-06-07 01:38:50 +0200

    style inheritance graphs for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,11 @@ extensions = ['sphinx.ext.autodoc',
 
 autodoc_member_order = 'bysource'
 
+inheritance_node_attrs = {'color': '"#465158"',
+                          'fontcolor': 'white',
+                          'fontsize': '13',
+                          'style': '"rounded,filled"'}
+
 intersphinx_mapping = {'python':   ('http://docs.python.org/', None),
                        'werkzeug': ('http://werkzeug.pocoo.org/docs/', None),
                        'logbook':  ('http://packages.python.org/Logbook/', None)}
