8f0ad9499decde190e053075a173cab26875fc04
Dag Odenhall <dag.odenhall@gmail.com>
2011-06-08 13:56:14 +0200

    monkeypatch werkzeug's log for logbook

--- pavement.py
+++ pavement.py
@@ -23,12 +23,21 @@ options(
           ('production', 'P', 'set debug to false')])
 def serve():
     """Run the development server."""
-    opts = options.serve
+    from logbook.compat   import redirect_logging
+    redirect_logging()
 
-    from werkzeug.serving import run_simple
+    from werkzeug         import _internal
     from relvlast         import Relvlast
 
+    opts = options.serve
     app = Relvlast(debug=not opts.production)
+
+    def _log(type, message, *args, **kwargs):
+        getattr(app.log, type)(message % args % kwargs)
+
+    _internal._log = _log
+
+    from werkzeug.serving import run_simple
     run_simple(opts.hostname, int(opts.port), app,
                use_reloader = not opts.no_reloader,
                use_debugger = not opts.no_debugger,
