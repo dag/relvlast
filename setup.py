fe17807de7e1d49eb44fd65950d73fd0f5a6a52e
Dag Odenhall <dag.odenhall@gmail.com>
2011-06-24 19:03:02 +0200

    list dependencies in setup.py for rtd

--- setup.py
+++ setup.py
@@ -4,6 +4,21 @@ from setuptools import setup, find_packages
 setup(
     name='Ramverk',
     packages=find_packages(exclude=('deployments', 'tests')),
+
+    install_requires=[
+        'Babel',
+        'Genshi',
+        'Logbook'
+        'Paver',
+        'PyYAML',
+        'Werkzeug',
+        'ZODB3',
+        'compactxml',
+        'creoleparser',
+        'pyScss',
+        'venusian',
+    ],
+
     entry_points = {
         'pygments.lexers': [
             'compactxml = ramverk.pygments:CompactXmlLexer',
