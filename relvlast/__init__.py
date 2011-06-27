8724e5ce446092f64758df8b98c5590e13969165
Dag Odenhall <dag.odenhall@gmail.com>
2011-06-27 23:55:04 +0200

    update relvlast for recent changes

--- relvlast/__init__.py
+++ relvlast/__init__.py
@@ -1,64 +1,75 @@
 from __future__          import absolute_import
+from pkg_resources       import resource_filename
 
 from relvlast.babel      import patch
 patch()
 
-from pkg_resources       import resource_filename
-from flatland.out.genshi import setup as setup_flatland
-from genshi.filters      import Translator
-from creoleparser        import Parser, creole11_base
-from werkzeug.local      import LocalProxy
-from werkzeug.routing    import Rule
 from babel               import Locale
 from babel.support       import Translations
-from ramverk.fullstack   import Application
-from ramverk.utils       import request_property
+from creoleparser        import Parser, creole11_base
+from flatland.out.genshi import setup as setup_flatland
+from genshi.filters      import Translator
+from werkzeug.local      import LocalProxy
+from werkzeug.routing    import Rule
+from werkzeug.utils      import cached_property
+from ramverk             import fullstack
+from ramverk.utils       import delegated_property
 from relvlast.objects    import Root
 
 
-class Relvlast(Application):
+class Environment(fullstack.Environment):
+
+    @cached_property
+    def locale(self):
+        return Locale(self.segments.get('locale', 'jbo'))
+
+    @cached_property
+    def db(self):
+        return self.persistent.setdefault('root', Root())
+
+    @cached_property
+    def translations(self):
+        return self.db.translations[self.locale.language]
+
+    @cached_property
+    def message_catalog(self):
+        dirname = resource_filename(self.application.module, 'translations')
+        return Translations.load(dirname, [self.locale])
+
+    @cached_property
+    def creole_parser(self):
+        return Parser(creole11_base(
+            wiki_links_base_url=self.path(':index')))
+
+
+class TemplateContext(fullstack.TemplateContext):
+
+    creole = delegated_property('environment.creole_parser.generate')
+
+    locale = delegated_property('environment.locale')
+
+    _ = delegated_property('environment.message_catalog.gettext')
+
+    def locale_name(self, locale):
+        return self.locale.languages.get(locale, Locale(locale).display_name)
+
+
+class Relvlast(fullstack.Application):
+
+    environment = Environment
+
+    template_context = TemplateContext
 
     def configure(self):
         self.url_map.add(Rule('/', redirect_to='jbo'))
         self.scan('relvlast.frontend', submount='/<locale>')
         self.scan('relvlast.dictionary', submount='/<locale>/vlaste')
 
-    @request_property
-    def locale(self):
-        return Locale(self.segments.get('locale', 'jbo'))
-
-    @request_property
-    def db(self):
-        return self.persistent.setdefault('root', Root())
-
-    @property
-    def translations(self):
-        return self.db.translations[self.locale.language]
-
-    def update_template_context(self, context):
-        super(Relvlast, self).update_template_context(context)
-        context.setdefault('creole', self.creole_parser.generate)
-        context.setdefault('locale', self.locale)
-        context.setdefault('locale_name', self.locale_name)
-        context.setdefault('_', self.message_catalog.gettext)
-
     def configure_genshi_template(self, template):
         setup_flatland(template)
-        Translator(LocalProxy(lambda: self.message_catalog)).setup(template)
-
-    @request_property
-    def message_catalog(self):
-        dirname = resource_filename(self.module, 'translations')
-        return Translations.load(dirname, [self.locale])
-
-    @request_property
-    def creole_parser(self):
-        return Parser(creole11_base(
-            wiki_links_base_url=self.path(':index')))
+        catalog = LocalProxy(lambda: self.local.message_catalog)
+        Translator(catalog).setup(template)
 
     def update_endpoint_values(self, endpoint, values):
         if self.url_map.is_endpoint_expecting(endpoint, 'locale'):
-            values.setdefault('locale', self.locale.language)
-
-    def locale_name(self, locale):
-        return self.locale.languages.get(locale, Locale(locale).display_name)
+            values.setdefault('locale', self.local.locale.language)
